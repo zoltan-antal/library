10f5ac18f25b41ee04716465035795d9f9eb546d
zoltan-antal <zoltan_antal@outlook.com>
2023-07-04 11:07:11 +0200

    Change from constructor to class syntax

--- script.js
+++ script.js
@@ -29,16 +29,18 @@ addBookForm.addEventListener("submit", (e) => {
 
 let myLibrary = [];
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+  toggleRead() {
+    this.read = !this.read;
+  }
 }
 
-Book.prototype.toggleRead = function() {
-  this.read = !this.read;
-}
 
 function addBookToLibrary(title, author, pages, read) {
   const newBook = new Book(title, author, pages, read);
